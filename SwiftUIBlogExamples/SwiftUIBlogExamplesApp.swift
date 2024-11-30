b0a401d74ab50740677109fd30758393bb27eca8
Theordius <87307483+Theordius@users.noreply.github.com>
2024-11-30 13:47:54 +0100

    Initial Commit

---
+++ SwiftUIBlogExamples/SwiftUIBlogExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBlogExamplesApp.swift
+//  SwiftUIBlogExamples
+//
+//  Created by Rafał Gęsior on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBlogExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
